49be0c589a342e53b58c9c8a6b72058aca58a747
Alex Goncalves <schroedergoncalves@gmail.com>
2021-09-11 18:08:09 -0400

    added a minutely forboding get method at API root

--- src/paths.js
+++ src/paths.js
@@ -10,6 +10,11 @@ const server = express()
 server.use(express.json())
 server.use(cors())
 
+/* Get Welcome */
+server.get('/', (req, res, next) => {
+    res.json("Hi there. It seems you have found my API.")
+})
+
 /* Get All Messages */
 server.get('/messages/all', (req, res, next) => {
     messages.findAll()
